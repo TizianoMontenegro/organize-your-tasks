c425b6d1a9884d7fc292a9405d19a4adaffcfa08
Tiziano Montenegro <91438170+tizielpro@users.noreply.github.com>
2022-10-19 01:59:38 +0200

    1.1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
 let form = document.getElementById("form__todo");
-let formSubmit = document.getElementById("form__submit");
-let formTasks = document.getElementById("form__tasks");
 
-function addTasks(e){
+function setTasks(e){
+    e.preventDefault()
     let formTitle = document.getElementById("input__title").value;
     let formDescription = document.getElementById("input__description").value;
     
-    const tasks = {
+    const task = {
         formTitle,
         formDescription
     };
+    if(localStorage.getItem("tasks") === null){
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+    }else{
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        tasks.push(task);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+    }
 
-    e.preventDefault()
-    console.log(tasks)
+    getTasks();
+};
+function getTasks(){
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    let listTasks = document.getElementById("tasks");
+    
+    listTasks.innerHTML = "";
+
+    for(let i = 0; i < tasks.length; i++){
+        let title = tasks[i].formTitle;
+        let description = tasks[i].formDescription;
+        listTasks.innerHTML += `<div class="card__tasks">
+            <p>${title} - ${description}</p>
+            <button id="remove" onclick="removeTask('${title}');">Remove</button>
+        </div>`;
+    }
+};
+function removeTask(title){
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    for(let i = 0; i < tasks.length; i++){
+        if(tasks[i].formTitle == title){
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+    getTasks();
 }
-form.addEventListener("submit", addTasks);
+form.addEventListener("submit", setTasks);
+getTasks();
